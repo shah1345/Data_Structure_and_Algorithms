2f86acf77f3ed7becdb805f49b0a2d2dbe07748b
Shah Newaz <34304584+shah1345@users.noreply.github.com>
2021-12-26 10:15:09 +0600

    Update bublesort.cpp

--- bublesort.cpp
+++ bublesort.cpp
@@ -44,3 +44,63 @@ int main(){
     printArray(array, length);
     cout<<endl;
 }
+
+
+
+#include <iostream>
+
+using namespace std;
+void swap(int*,int*);
+
+
+void bubble_sort(int arr[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(&arr[j],&arr[j+1]);
+            }
+        }
+    }
+}
+void display(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    int n;
+    cout<<"enter the size of the array:"<<endl;
+    cin>>n;
+    int array_of_numbers[n];
+    cout<<"enter the elements of the array"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>array_of_numbers[i];
+    }
+    cout<<"array as it was entered:"<<endl;
+    display(array_of_numbers,n);
+    bubble_sort(array_of_numbers,n);
+    cout<<"array after bubble sort:"<<endl;
+    display(array_of_numbers,n);
+    return 0;
+}
+void swap(int *a,int *b)
+{
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+
+
+
+
